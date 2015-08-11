6619f7d338fa0525d456348a634b005770c84330
Roland Tritsch <roland@tritsch.org>
2015-08-11 14:26:20 +0100

    First round of playing (data, control, functions, ...)

--- hello.go
+++ hello.go
@@ -6,5 +6,14 @@ import (
 )
 
 func main() {
-	fmt.Printf(stringutil.Reverse("\nHello, dnalor!"))
+	fmt.Println(play.Reverse("Hello, dnalor!"))
+
+	fmt.Println(play.RunControl("Roland", 50, 50))
+	fmt.Println(play.RunControl2("Roland", 50, 10))
+
+	fmt.Println(play.RunSwitch(0))
+	fmt.Println(play.RunSwitch2())
+
+	fmt.Println(play.RunFunction(1))
+	fmt.Println(play.RunFunction2("Roland"))
 }
